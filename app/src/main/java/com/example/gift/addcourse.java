f7d653a35dc5c775e9625df05e672981c5c4f016
Lantao Cui <clt2738461982@gmail.com>
2019-01-18 15:17:26 -0500

    second commit

--- app/src/main/java/com/example/gift/addcourse.java
+++ app/src/main/java/com/example/gift/addcourse.java
@@ -1,8 +1,12 @@
 package com.example.gift;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.webianks.library.scroll_choice.ScrollChoice;
 
@@ -11,20 +15,38 @@ import java.util.List;
 
 public class addcourse extends AppCompatActivity {
     List<String> datas = new ArrayList<>();
+    List<String> avaliable_times = new ArrayList<>();
+    List<String> backup_times = new ArrayList<>();
     TextView textView;
     ScrollChoice scrollChoice;
+    boolean add = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcourse);
+        Button confirm = findViewById(R.id.confirm);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!(avaliable_times.contains(backup_times.get(backup_times.size()-1)))) {
+                    avaliable_times.add(backup_times.get(backup_times.size() - 1));
+                    Context context = getApplicationContext();
+                    CharSequence text = "Succesfully added " + backup_times.get(backup_times.size() - 1);
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            }
+        });
         initViews();
         loadDatas();
         scrollChoice.addItems(datas,2);
         scrollChoice.setOnItemSelectedListener(new ScrollChoice.OnItemSelectedListener() {
             @Override
             public void onItemSelected(ScrollChoice scrollChoice, int position, String name) {
-                textView.setText("Choice " +name);
+                textView.setText("Choice " + name);
+                backup_times.add(name);
             }
         });
     }
