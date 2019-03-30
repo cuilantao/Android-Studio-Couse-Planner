31b779918e9e95160b12e590ccbfb6a5f0b5a566
Lantao Cui <clt2738461982@gmail.com>
2019-03-29 20:14:31 -0400

    Manage data

---
+++ app/src/main/java/com/example/gift/fivedays.java
@@ -0,0 +1,9 @@
+package com.example.gift;
+
+public class fivedays {
+    static boolean mon[] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false};
+    static boolean tue[] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false};
+    static boolean wed[] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false};
+    static boolean thur[] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false};
+    static boolean fri[] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false};
+}
--- app/src/main/java/com/example/gift/timetable.java
+++ app/src/main/java/com/example/gift/timetable.java
@@ -6,26 +6,56 @@ import java.sql.Time;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.lang.String;
 import java.util.Set;
 
 public class timetable {
-    private ArrayList<course> monday =  new ArrayList<>();
-    private ArrayList<course> tuesday =  new ArrayList<>();
-    private ArrayList<course> wednesday =  new ArrayList<>();
-    private ArrayList<course> thursday =  new ArrayList<>();
-    private ArrayList<course> friday =  new ArrayList<>();
-    private ArrayList<course> saturday =  new ArrayList<>();
-    private ArrayList<course> sunday =  new ArrayList<>();
-    private HashMap<String, ArrayList<course>> coursemap = new HashMap<>();
     public timetable(course_manager courses){
+        HashMap<String, HashMap<String, ArrayList<Integer>>> code = new HashMap<>();
         for (int i=0; i<courses.get_list().size(); i++){
-            Iterator<String> w = courses.get_list().get(i).time.keySet().iterator();
-            ArrayList<String> tmp = new ArrayList<>();
-            while (w.hasNext()){
-                tmp.add(w.next());
+            String course_name = courses.get_list().get(i).name;
+            if (!(code.containsKey(course_name))){
+                HashMap<String,ArrayList<Integer>> tmp = new HashMap<>();
+                ArrayList<Integer> tmp1 = new ArrayList<>();
+                fill_array(tmp1, courses.get_list().get(i).time);
+                tmp.put(courses.get_list().get(i).lecture_code, tmp1);
+                code.put(course_name, tmp);
             }
-            AVLNode tmp1 = new AVLNode(courses.get_list().get(i).name, courses.get_list().get(i).lecture_code, tmp);
+            else{
+                ArrayList<Integer> tmp1 = new ArrayList<>();
+                fill_array(tmp1, courses.get_list().get(i).time);
+                code.get(course_name).put(courses.get_list().get(i).lecture_code, tmp1);
+            }
         }
+        System.out.println(code.get("CSC165").keySet());
+        System.out.println(code.get("CSC148").keySet());
+        for (int i=0; i<code.get("CSC165").get("0101").size();i++){
+            System.out.println(code.get("CSC165").get("0101").get(i));
+        }
+        for (int i=0; i<code.get("CSC165").get("0201").size();i++){
+            System.out.println(code.get("CSC165").get("0201").get(i));
+        }
+        for (int i=0; i<code.get("CSC148").get("0101").size();i++){
+            System.out.println(code.get("CSC148").get("0101").get(i));
+        }
+    }
+
+    public void fill_array(ArrayList<Integer> tmp1, HashMap<String, String> course_time){
+        Iterator<String> w = course_time.keySet().iterator();
+        while (w.hasNext()){
+            tranform(course_time.get(w.next()), tmp1);
+        }
+    }
+    public void tranform(String time, ArrayList<Integer> tmp1){
+        int slice = time.indexOf(":");
+        String sub1 = time.substring(0, slice);
+        Integer start = Integer.parseInt(sub1);
+        int middle = time.indexOf("-");
+        int slice2 = time.lastIndexOf(":");
+        String sub2 = time.substring(middle+1, slice2);
+        Integer end = Integer.parseInt(sub2);
+        tmp1.add(start);
+        tmp1.add(end);
     }
 /*    public boolean append_course(course A){
         if (detect_collision(A)){
