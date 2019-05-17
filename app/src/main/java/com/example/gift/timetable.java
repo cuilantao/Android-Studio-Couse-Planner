b059c80743faa6f06ebebb60712c69b07494786f
Lantao Cui <clt2738461982@gmail.com>
2019-05-17 15:11:16 -0400

    sort class

--- app/src/main/java/com/example/gift/timetable.java
+++ app/src/main/java/com/example/gift/timetable.java
@@ -4,30 +4,31 @@ import android.os.HardwarePropertiesManager;
 
 import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.lang.String;
 import java.util.Set;
 
 public class timetable {
-    int[] Monday = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
-    int[] Tuesday = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
-    int[] Wednesday = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
-    int[] Thursday = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
-    int[] Friday = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
-    public timetable(course_manager courses){
+    int[] Monday = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int[] Tuesday = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int[] Wednesday = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int[] Thursday = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int[] Friday = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    public timetable(course_manager courses) {
         HashMap<String, HashMap<String, HashMap<String, ArrayList<Integer>>>> code = new HashMap<>();
-        for (int i=0; i<courses.get_list().size(); i++){
+        for (int i = 0; i < courses.get_list().size(); i++) {
             String course_name = courses.get_list().get(i).name;
-            if (!(code.containsKey(course_name))){
-                HashMap<String,HashMap<String,ArrayList<Integer>>> tmp = new HashMap<>();
+            if (!(code.containsKey(course_name))) {
+                HashMap<String, HashMap<String, ArrayList<Integer>>> tmp = new HashMap<>();
                 HashMap<String, ArrayList<Integer>> weekday_to_time = new HashMap<>();
                 ArrayList<Integer> tmp1 = new ArrayList<>();
                 fill_array(weekday_to_time, courses.get_list().get(i).time);
                 tmp.put(courses.get_list().get(i).lecture_code, weekday_to_time);
                 code.put(course_name, tmp);
-            }
-            else{
+            } else {
                 ArrayList<Integer> tmp1 = new ArrayList<>();
                 HashMap<String, ArrayList<Integer>> weekday_to_time = new HashMap<>();
                 fill_array(weekday_to_time, courses.get_list().get(i).time);
@@ -36,39 +37,46 @@ public class timetable {
         }
         Iterator<String> all_course = code.keySet().iterator();
         String first = all_course.next();
-        sort(code, first, Monday, Tuesday, Wednesday, Thursday, Friday); // a recursive planning function
+        System.out.println(sort(code, first, Monday, Tuesday, Wednesday, Thursday, Friday)); // a recursive planning function
     }
 
-    public void fill_array(HashMap<String, ArrayList<Integer>> weekday_to_time, HashMap<String, String> course_time){
+    public void view (int[] tmp){
+        for (int i=0;i<tmp.length;i++){
+            System.out.println(tmp[i]);
+        }
+    }
+    public void fill_array(HashMap<String, ArrayList<Integer>> weekday_to_time, HashMap<String, String> course_time) {
         Iterator<String> w = course_time.keySet().iterator();
-        while (w.hasNext()){
+        while (w.hasNext()) {
             ArrayList<Integer> tmp1 = new ArrayList<>();
             String tmp = w.next();
             tranform(course_time.get(tmp), tmp1);
             weekday_to_time.put(tmp, tmp1);
         }
     }
-    public void tranform(String time, ArrayList<Integer> tmp1){
+
+    public void tranform(String time, ArrayList<Integer> tmp1) {
         int slice = time.indexOf(":");
         String sub1 = time.substring(0, slice);
         Integer start = Integer.parseInt(sub1);
         int middle = time.indexOf("-");
         int slice2 = time.lastIndexOf(":");
-        String sub2 = time.substring(middle+1, slice2);
+        String sub2 = time.substring(middle + 1, slice2);
         Integer end = Integer.parseInt(sub2);
         tmp1.add(start);
         tmp1.add(end);
     }
+
     public int sort(HashMap<String, HashMap<String, HashMap<String, ArrayList<Integer>>>> code, String course_name,
-                        int[] Monday, int[] Tuesday,
-                        int[] Wednesday, int[] Thursday, int[] Friday){
+                    int[] Monday, int[] Tuesday,
+                    int[] Wednesday, int[] Thursday, int[] Friday) {
         HashMap<String, HashMap<String, ArrayList<Integer>>> next = code.get(course_name);
         Iterator<String> lecture_codes = next.keySet().iterator();
         while (lecture_codes.hasNext()) {
             String next_lect = lecture_codes.next();
             HashMap<String, ArrayList<Integer>> next_next = next.get(next_lect);
             Iterator<String> time = next_next.keySet().iterator();
-            reset(code.get(course_name), next_lect, Monday, Tuesday, Wednesday, Thursday, Friday);//we know the current lecture section
+            reset(code.get(course_name), next_lect, Monday, Tuesday, Wednesday, Thursday, Friday);//we know the current lecture section, we want to reset the previous one
             int[] monday_tmp = Monday.clone();
             int[] tuesday_tmp = Tuesday.clone();
             int[] wednesday_tmp = Wednesday.clone();
@@ -103,25 +111,32 @@ public class timetable {
                     if (rec_next == "NULL") {
                         return 1;
                     }
-                    sort(code,rec_next, Monday, Tuesday, Wednesday, Thursday, Friday);
+                    if (sort(code, rec_next, Monday, Tuesday, Wednesday, Thursday, Friday) == 1) {
+                        return 1;
+                    }
                 }
             }
         }
-        return 1;//should never get to this line;
+        reset(code.get(course_name), "", Monday, Tuesday, Wednesday, Thursday, Friday);
+        return 0;
     }
+
     public boolean set_array(int[] day, int start, int end) {
-        for (int i = start; i<end+1;i++) {
+        for (int i = start+1; i < end + 1; i++) {
             if (day[i - 9] == 1) {
-                return false;
+                if (i != end) {
+                    return false;
+                }
             }
             day[i - 9] = 1;
         }
-    return true;
+        return true;
     }
-    public String get_next_class(Iterator<String> iter, String current){
-        while (iter.hasNext()){
-            if (iter.next() == current){
-                if (iter.hasNext()){
+
+    public String get_next_class(Iterator<String> iter, String current) {
+        while (iter.hasNext()) {
+            if (iter.next() == current) {
+                if (iter.hasNext()) {
                     return iter.next();
                 }
                 return "NULL";
@@ -130,33 +145,70 @@ public class timetable {
         return "Error";
     }
 
-    public void reset(HashMap<String, HashMap<String, ArrayList<Integer>>> time, String next_lect, int[] Monday, int[] Tuedsday, int[] Wednesday, int[] Thursday, int[] Friday){
-        Iterator<String> lectures= time.keySet().iterator();
-        while (lectures.hasNext()){
-            HashMap<String, ArrayList<Integer>> weekday = time.get(lectures.next());
-            Iterator<String> clear = weekday.keySet().iterator();
-            while(clear.hasNext()){
-                String p = clear.next();
-                int i = weekday.get(p).get(0);
-                int j = weekday.get(p).get(1);
-                if (p == "Monday"){
-                    clear(Monday,i,j);
-                }
-                else if (p == "Tuesday"){
-                    clear(Tuedsday,i,j);
-                }
-                else if (p == "Wednesday"){
-                    clear(Wednesday,i,j);
-                }
-                else if (p == "Thursday"){
-                    clear(Thursday,i,j);
-                }
-                else{
-                    clear(Friday,i,j);
+    public void reset(HashMap<String, HashMap<String, ArrayList<Integer>>> time, String next_lect, int[] Monday, int[] Tuedsday, int[] Wednesday, int[] Thursday, int[] Friday) {
+        if (next_lect == ""){
+            Iterator<String> lectures = time.keySet().iterator();
+            while (lectures.hasNext()){
+                String cur = lectures.next();
+                HashMap<String, ArrayList<Integer>> cur1 = time.get(cur);
+                Iterator<String> cur2 = cur1.keySet().iterator();
+                while (cur2.hasNext()) {
+                    String cur3 = cur2.next();
+                    int i = cur1.get(cur3).get(0);
+                    int j = cur1.get(cur3).get(1);
+                    if (cur3 == "Monday") {
+                        clear(Monday, i, j);
+                    } else if (cur3 == "Tuesday") {
+                        clear(Tuedsday, i, j);
+                    } else if (cur3 == "Wednesday") {
+                        clear(Wednesday, i, j);
+                    } else if (cur3 == "Thursday") {
+                        clear(Thursday, i, j);
+                    } else {
+                        clear(Friday, i, j);
+                    }
                 }
             }
         }
-
+        else {
+            Iterator<String> lectures = time.keySet().iterator();
+            String w = "";
+            String p = "";
+            while (true) {// look for previous one
+                if (lectures.hasNext()) {
+                    p = lectures.next();
+                    if (lectures.hasNext()) {
+                        w = lectures.next();
+                        if (w != next_lect) {
+                            w = "";
+                        } else {
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+            if (w != "") {
+                HashMap<String, ArrayList<Integer>> weekday = time.get(p);
+                Iterator<String> clear = weekday.keySet().iterator();
+                while (clear.hasNext()) {
+                    String sec = clear.next();
+                    int i = weekday.get(sec).get(0);
+                    int j = weekday.get(sec).get(1);
+                    if (sec == "Monday") {
+                        clear(Monday, i, j);
+                    } else if (sec == "Tuesday") {
+                        clear(Tuedsday, i, j);
+                    } else if (sec == "Wednesday") {
+                        clear(Wednesday, i, j);
+                    } else if (sec == "Thursday") {
+                        clear(Thursday, i, j);
+                    } else {
+                        clear(Friday, i, j);
+                    }
+                }
+            }
+        }
     }
     public void clear(int [] day, int start, int end){
         for (int i = start; i<end+1;i++) {
